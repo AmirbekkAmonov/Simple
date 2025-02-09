db4e5d8eae09938d50a241a9bbfce7cc39deceff
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-02-09 15:00:20 +0500

    section qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header"
 import Hero from "./components/sections/Hero/Hero"
 import Explore from "./components/sections/Explore/Explore"
 import Works from "./components/sections/Works/Works"
+import World from "./components/sections/World/World"
 import Footer from "./components/Footer/Footer"
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Explore />
       <Works /> 
+      <World />
       <Footer />
     </>
   )
